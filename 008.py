83bab144d656850ca8dc03f9b692573fc1fc2a3f
menmasaraa <menmasaraa16@gmail.com>
2023-07-26 01:49:12 -0700

    if~else done

---
+++ 008.py
@@ -0,0 +1,7 @@
+print(1, 2, 3)
+print(3, 4, 5)
+
+print(end=" ")
+# compare
+print(1, 2, 3, end=" ")
+print(3, 4, 5)
---
+++ 009.py
@@ -0,0 +1,28 @@
+'''
+# 10 % 2 == 0
+# 5 % 2 == 1
+
+import sys 
+
+inpurNum = int(input("input integeer : "))
+
+if inputNum <= 0:
+    print("This is Negative Num.")
+    print("Program exit.")
+    sys.exit() # system exit
+else:
+    if inputNum % 2 == 0:
+        print("Even Num")
+    else:
+        print("Odd Num")
+        '''
+# solve
+score = int(input("test score :))
+if score >= 90 :
+    print('A')
+elif score >=80 :
+    print('B')
+elif score >=70 :
+    print('C')
+
+        
